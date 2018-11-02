870bb8ec345c5670e639fdb5af56cad2b35cb77b
Sooyoung Kim <sooyoung10@gmail.com>
2018-11-01 22:53:57 -0400

    menu bar created

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { LinksComponent } from './links/links.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MusicComponent } from './music/music.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    LinksComponent,
+    HomeComponent,
+    AboutComponent,
+    MusicComponent,
+    PhotosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'links',
+        component: LinksComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'photos',
+        component: PhotosComponent
+      },
+      {
+        path: 'music',
+        component: MusicComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
